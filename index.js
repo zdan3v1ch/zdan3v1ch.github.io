652b05a21edd8038bab2037bd477aa3553218cfa
Anatoli Zdanevich <anatoli.zdanevitch@gmail.com>
2024-10-14 17:02:30 +0300

    feat: add new medecine

--- index.js
+++ index.js
@@ -1,95 +1,6 @@
 const body = document.querySelector('body');
 
-const myListOfMedicines = [
-  {
-    name: 'Аскорбиновая кислота',
-    expireTime: '01-04-2027',
-  },
-  {
-    name: 'Витавирин',
-    expireTime: '01-11-2025',
-  },
-  {
-    name: 'Фенкарол',
-    expireTime: '01-03-2026',
-  },
-  {
-    name: 'Шприц инъекционный(2 шт.)',
-    expireTime: '01-07-2027',
-  },
-  {
-    name: 'Флоксал',
-    expireTime: '01-12-2024',
-  },
-  {
-    name: 'Лазолван сироп',
-    expireTime: '01-10-2024',
-  },
-  {
-    name: 'Наклофен',
-    expireTime: '01-11-2026',
-  },
-  {
-    name: 'Гевиран',
-    expireTime: '01-12-2024',
-  },
-  {
-    name: 'Артелак ночной',
-    expireTime: '01-03-2025',
-  },
-  {
-    name: 'Мидокалм',
-    expireTime: '01-09-2026',
-  },
-  {
-    name: 'Мелбек',
-    expireTime: '01-07-2027',
-  },
-  {
-    name: 'Ультрафастин',
-    expireTime: '01-11-2025',
-  },
-  {
-    name: 'Миконазол',
-    expireTime: '01-05-2026',
-  },
-  {
-    name: 'Гепарин',
-    expireTime: '01-04-2025',
-  },
-  {
-    name: 'Нимесил',
-    expireTime: '01-08-2026',
-  },
-  {
-    name: 'Парлазин Нео',
-    expireTime: '01-06-2026',
-  },
-  {
-    name: 'Ринодекса',
-    expireTime: '01-03-2027',
-  },
-  {
-    name: 'Лиотон',
-    expireTime: '01-01-2025',
-  },
-  {
-    name: 'Никсар',
-    expireTime: '01-11-2026',
-  },
-  {
-    name: 'Нафазолин',
-    expireTime: '01-05-2025',
-  },
-  {
-    name: 'Нозакар гель мята',
-    expireTime: '01-07-2025',
-  },
-  {
-    name: 'Цитрамон-Боримед (2шт)',
-    expireTime: '01-07-2028',
-  }
-]
+import { myListOfMedicines } from '/list.js'
 
 const sortArray = myListOfMedicines.sort((a, b) => {
   if (a.name < b.name) {
@@ -125,7 +36,7 @@ sortArray.forEach((el) => {
   expire.classList.add('expire')
   name.textContent = el.name;
   expire.textContent = `до: ${el.expireTime}`;
-  if(checkDate(el.expireTime)) {
+  if (checkDate(el.expireTime)) {
     expire.classList.add('red');
   }
   medicineBlock.append(name, expire);
