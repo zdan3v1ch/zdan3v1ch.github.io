261b5b6fde7b1c0be60ca6e6900619479d519c9b
Anatoli Zdanevich <anatoli.zdanevitch@gmail.com>
2025-09-07 22:19:16 +0300

    feat: add new medicines

--- list.js
+++ list.js
@@ -1,88 +1,128 @@
 export const myListOfMedicines = [
   {
-    name: 'Аскорбиновая кислота',
-    expireTime: '01-04-2027',
+    name: "Аскорбиновая кислота",
+    expireTime: "01-04-2027",
   },
   {
-    name: 'Витавирин',
-    expireTime: '01-11-2025',
+    name: "Витавирин",
+    expireTime: "08-2026",
   },
   {
-    name: 'Фенкарол',
-    expireTime: '01-03-2026',
+    name: "Фенкарол",
+    expireTime: "01-03-2026",
   },
   {
-    name: 'Шприц инъекционный(2 шт.)',
-    expireTime: '01-07-2027',
+    name: "Шприц инъекционный(2 шт.)",
+    expireTime: "01-07-2027",
   },
   {
-    name: 'Наклофен',
-    expireTime: '01-11-2026',
+    name: "Наклофен",
+    expireTime: "01-11-2026",
   },
-  
+
   {
-    name: 'Мидокалм',
-    expireTime: '01-09-2026',
-  },
-  {
-    name: 'Мелбек',
-    expireTime: '01-07-2027',
+    name: "Мидокалм",
+    expireTime: "01-09-2026",
   },
   {
-    name: 'Ультрафастин',
-    expireTime: '01-11-2025',
+    name: "Мелбек",
+    expireTime: "01-07-2027",
   },
   {
-    name: 'Миконазол',
-    expireTime: '01-05-2026',
-  },
-  
-  {
-    name: 'Нимесил',
-    expireTime: '01-08-2026',
+    name: "Ультрафастин",
+    expireTime: "01-11-2025",
   },
   {
-    name: 'Парлазин Нео',
-    expireTime: '01-06-2026',
+    name: "Миконазол",
+    expireTime: "01-05-2026",
+  },
+
+  {
+    name: "Нимесил",
+    expireTime: "01-08-2026",
   },
   {
-    name: 'Ринодекса',
-    expireTime: '01-03-2027',
-  },
-  
-  {
-    name: 'Никсар',
-    expireTime: '01-11-2026',
-  },
-  
-  {
-    name: 'Цитрамон-Боримед',
-    expireTime: '01-07-2028',
+    name: "Парлазин Нео",
+    expireTime: "01-06-2026",
   },
   {
-    name: 'Ангримакс',
-    expireTime: '01-07-2026',
+    name: "Ринодекса",
+    expireTime: "01-03-2027",
+  },
+
+  {
+    name: "Никсар",
+    expireTime: "01-11-2026",
+  },
+
+  {
+    name: "Цитрамон-Боримед",
+    expireTime: "01-07-2028",
   },
   {
-    name: 'Нифуроксазид',
-    expireTime: '10-2026',
-  },{
-    name: 'Оптинол',
-    expireTime: '10-2026',
-  },{
-    name: 'Пепсан-Р (2пакета)',
-    expireTime: '09-2026',
-  },{
-    name: 'Уголь 2 пластинки',
-    expireTime: '07-2027',
-  },{
-    name: 'Ацикловир',
-    expireTime: '08-2027',
-  },{
-    name: 'Моменекс',
-    expireTime: '11-2027',
-  },{
-    name: 'Дукресса',
-    expireTime: '11-2026',
+    name: "Ангримакс",
+    expireTime: "01-07-2026",
   },
-]
+  {
+    name: "Нифуроксазид",
+    expireTime: "10-2026",
+  },
+  {
+    name: "Оптинол",
+    expireTime: "10-2026",
+  },
+  {
+    name: "Пепсан-Р (2пакета)",
+    expireTime: "09-2026",
+  },
+  {
+    name: "Уголь 2 пластинки",
+    expireTime: "07-2027",
+  },
+  {
+    name: "Ацикловир",
+    expireTime: "08-2027",
+  },
+  {
+    name: "Моменекс",
+    expireTime: "11-2027",
+  },
+  {
+    name: "Дукресса",
+    expireTime: "11-2026",
+  },
+
+  {
+    name: "Драмина",
+    expireTime: "03-2030",
+  },
+
+  {
+    name: "Септолете",
+    expireTime: "01-2029",
+  },
+  {
+    name: "Гидрокортизон",
+    expireTime: "01-2027",
+  },
+  {
+    name: "Фуразолид",
+    expireTime: "03-2028",
+  },
+  {
+    name: "Лопирамида гидрохлорид",
+    expireTime: "05-2028",
+  },
+  {
+    name: "Ацикловир (табл)",
+    expireTime: "04-2028",
+  },
+  {
+    name: "Гростудин",
+    expireTime: "01-2027",
+  },
+  {
+    name: "Пластырь разный",
+    expireTime: "01-2037",
+  },
+];
